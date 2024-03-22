5fae588e1b6de04de1c4be87002411320971ac3c
TOMAS NEIMERMAN GROSSO <47496739@ort>
2024-03-22 12:00:25 -0300

    1

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,10 +1,33 @@
 import express, { response } from "express";
+import {UserService} from "../servicios/user-service.js/"
 const router = express.Router();
+const eventService = new EventService();
 
-router.get("/", (request,response) => {
-    return response.json([
 
-        
-    ])
-
-})
+router.get("/", (request, response) => {
+    const limit = request.query.limit;
+    const offset = request.query.offset;
+  
+    try {
+      const allEvents = eventService.getAllEvents(pageSize, page);
+      return response.json(allEvents);
+    } catch (error) {
+      console.log("Un Error");
+      return response.json("Un Error");
+    }
+  })
+  
+  
+  
+  router.post("/", (req, res) => {
+    const body = req.body;
+    console.log(body);
+    return res.status(201).send({
+        id: 1,
+        first_name: body.first_name,
+        last_name: body.last_name,
+        username: body.username,
+        password: body.password
+    });
+  });
+export default router;
--- src/servicios/province-service.js
+++ src/servicios/province-service.js
@@ -15,6 +15,7 @@ export class ProvinceService {
                 total: query2,
             },
         }
+        
     }
     
 }
