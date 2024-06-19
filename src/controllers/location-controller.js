144fa7bf0f11ff7d373a5d8eac6eee320a663124
TOMAS NEIMERMAN GROSSO <47496739@ort>
2024-06-19 12:00:44 -0300

    12

--- src/controllers/location-controller.js
+++ src/controllers/location-controller.js
@@ -7,10 +7,17 @@ const router = Router()
 router.get("/",  async (request, response) => {
     const limit = request.query.limit;
     const offset = request.query.offset;
+    const id = request.params.id;
     try{
         const res = await locationService.getAllLocations(limit,offset);
         if(id != null){
-            
+            try{
+                const res = await locationService.locationById(id);
+                return response.status(201).json(res);
+            }catch(error){
+                console.log("Hubo un error:", error);
+                return response.status(400).json("No se pudo encontrar la localidad")
+            }
         }
         return response.status(201).json(res);
     }catch(error){
@@ -18,18 +25,5 @@ router.get("/",  async (request, response) => {
         return response.status(400).json("No se pudo mostrar las localidades")
     }
 })
-router.get("/:id", async (request,response) =>{
-    const id = request.params.id;
-    try{
-        const res = await locationService.locationById(id);
-        return response.status(201).json(res);
-    }catch(error){
-        console.log("Hubo un error:", error);
-        return response.status(400).json("No se pudo encontrar la localidad")
-    }
-})
-
-    
-    
 
 export default router;
